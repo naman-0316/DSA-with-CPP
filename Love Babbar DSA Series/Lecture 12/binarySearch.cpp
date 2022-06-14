7ffce43dbdb38512fe87255c9f705d688fc8257e
Naman Panjeta <pes1202102271@pesu.pes.edu>
2022-06-14 19:33:33 +0530

    Binary Search

---
+++ Love Babbar DSA Series/Lecture 12/binarySearch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key) {
+
+    int start = 0;
+    int end = size-1;
+
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+
+        if(arr[mid] == key) {
+            return mid;
+        }
+
+        //right part
+        if(key > arr[mid]) {
+            start = mid + 1;
+        }
+        else{ //key < arr[mid]
+            end = mid - 1;
+        }
+
+        mid = start + (end-start)/2;
+    }
+    
+    return -1;
+}
